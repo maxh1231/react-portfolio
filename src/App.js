55885a21cca6fa8dd1cff22ebc22516a11bf7829
maxh1231 <maxhumpherys@gmail.com>
2022-02-02 15:32:15 -0700

    project descriptions

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const projects = [
     id: 1,
     title: '#1Fan',
     img: no1fan,
-    disc: 'Music hub to listen, share and favorite your favorite songs and artists.',
+    disc: 'Music hub to listen, share and favorite your favorite songs and artists. Group project, built with Node.Js, Tailwind.Css, Express.Js, mySQL and Sequelize ODM. Deployed to Heroku.',
     repo: 'https://github.com/maxh1231/no1fan',
     live: 'https://no1fan.herokuapp.com/'
   },
@@ -25,7 +25,7 @@ const projects = [
     id: 2,
     title: 'Meet Landing Page',
     img: meet,
-    disc: 'Landing page for the Meet application, Landing page for the Meet application, Landing page for the Meet application, ',
+    disc: 'HTML and CSS challenge for the popular frontendmentor website. The challenge provides you images, hex color codes, and a wireframe. Showcases my CSS skills. ',
     repo: 'https://github.com/maxh1231/meet-landing-page',
     live: 'https://maxh1231.github.io/meet-landing-page/'
   },
@@ -33,7 +33,7 @@ const projects = [
     id: 3,
     title: 'Travel Agent',
     img: travelAgent,
-    disc: 'Plan easy travel by viewing hotel and weather information in a given area.',
+    disc: 'An application that assists you in making travel plans. Displays weather information and hotel information in a given city. Built with vanilla JavaScript, Moment.Js, HTML and CSS.',
     repo: 'https://github.com/maxh1231/travel-agent',
     live: 'https://maxh1231.github.io/travel-agent/'
   },
@@ -41,7 +41,7 @@ const projects = [
     id: 4,
     title: 'Weather Dashboard',
     img: dashboard,
-    disc: 'Weather forecast in a given city. Weather forecast in a given city. Weather forecast in a given city. Weather forecast in a given city.',
+    disc: 'An application that provides you the current weather information including wind speed, humidity percentage, and UV index. Additionally displays the upcoming 5-day weather forecast. Built with vanilla JavaScript, Moment.JS, 3 3rd party APIs, HTML and CSS. ',
     repo: 'https://github.com/maxh1231/weather-dashboard',
     live: 'https://maxh1231.github.io/weather-dashboard/'
   },
@@ -49,7 +49,7 @@ const projects = [
     id: 5,
     title: 'Tech Blog',
     img: techblog,
-    disc: 'Reddit esq tech blog, create and share tech posts, discuss tech with other users via comments',
+    disc: 'Forum application that allows user accounts to be created and logged into. Logged in users are able to view their dashboard containing posts created by them. Additionally, logged in users are able to comment on posts. Built with Node.Js, CSS, Express.js, mySQL and Sequelize ODM. Deployed to Heroku. ',
     repo: 'https://github.com/maxh1231/tech-blog',
     live: 'https://maxh-techblog.herokuapp.com/'
   },
@@ -57,7 +57,7 @@ const projects = [
     id: 6,
     title: 'Password Generator',
     img: passGen,
-    disc: 'Generate a password based on a given critera. Set length of password and types of characters.',
+    disc: 'Application that generates a random password based on a given set of criteras. Users have the ability to choose the length of the password, include numbers and special characters, and lowercase and uppercase letters if indicated. Built with vanilla javaScript. ',
     repo: 'https://github.com/maxh1231/password-generator',
     live: 'https://maxh1231.github.io/password-generator/',
   }
