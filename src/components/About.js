d8a4a05e857f9081bb6f3044290a0b58480d5023
maxh1231 <maxhumpherys@gmail.com>
2022-02-02 14:03:03 -0700

    about me styling

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,20 @@
+import photo from '../me-400x600.jpg'
+
 const About = () => {
     return (
-        <section>
-            <div>
-                <img src='' alt=''></img>
+        <section className='flex flex-row justify-center w-full h-screen bg-[#e5e5e5]'>
+            <div className="ml-12 w-1/4 flex flex-col justify-center">
+                <div>
+                    <h3 className="text-5xl text-left">Hi, I'm Max</h3>
+                </div>
+                <div>
+                    <p className="text-left text-lg">
+                        Full Stack web developer with a passion to code and learning new technologies. Proficiency in the MERN stack and always pursuing a deeper knowledge of all-things programming. View my projects to see my ability to develop well-functioning, clean user-interfaces and applications.
+                    </p>
+                </div>
             </div>
-            <div>
-                <p>
-                    Placeholder about me
-                </p>
+            <div className="mr-12 w-1/4 flex justify-center items-center">
+                <img className="avatar border-4 border-[#4D0823] rounded-full object-cover shadow-2xl" src={photo} alt='Picture of me' width=""></img>
             </div>
         </section>
     )
