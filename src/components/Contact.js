faceb6c366c8a0c74cf22c430d2211dfa2931ae8
maxh1231 <maxhumpherys@gmail.com>
2022-02-01 22:18:53 -0700

    contact section styles

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,20 +1,20 @@
 const Contact = () => {
     return (
-        <section className="flex flex-col justify-center items-center h-screen">
+        <section className="flex flex-col justify-center items-center h-screen bg-[#e5e5e5]">
             <div>
-                <div>
-                    <input type='text' placeholder='Name'></input>
+                <div className="my-4 w-[400px] shadow-lg">
+                    <input className="contact py-2 pl-2 w-full" type='text' placeholder='Name'></input>
                 </div>
-                <div>
-                    <input type='text' placeholder='Email'></input>
+                <div className="my-4 w-[400px] shadow-lg">
+                    <input className="contact py-2 pl-2 w-full" type='text' placeholder='Email'></input>
                 </div>
-                <div>
-                    <textarea placeholder="Message">
+                <div className="my-4 w-[400px] h-[150px] shadow-lg">
+                    <textarea className="contact py-2 pl-2 w-full h-full" placeholder="Message">
 
                     </textarea>
                 </div>
-                <div>
-                    <button>Submit</button>
+                <div className="my-4">
+                    <button className="btn py-2 px-6 bg-[#FFF] ">Submit</button>
                 </div>
             </div>
         </section>
