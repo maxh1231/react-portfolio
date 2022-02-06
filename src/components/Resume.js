a7e8d31eace1c271a7491729c79bab478dd1c095
maxh1231 <maxhumpherys@gmail.com>
2022-02-05 18:13:45 -0700

    contact section styles

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -2,14 +2,14 @@ import photo from '../resume.PNG';
 
 const Resume = () => {
     return (
-        <section>
+        <section className=' h-full bg-[#e5e5e5] flex flex-col items-center pt-2'>
             <div>
                 <img src={photo} alt=""></img>
             </div>
-            <div>
+            <div className='mt-2'>
 
                 <a href="https://docs.google.com/document/d/1S9RU3scZyjFcD91g1vVpjavVcvp9GqvFuitpEzQ5CCA/edit?usp=sharing">
-                    <button>
+                    <button className='btn py-2 px-6 bg-[#FFF]'>
                         View Resume
                     </button>
                 </a>
