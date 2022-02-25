fea0a394d802d4a96d00dedcef34fa607da295aa
maxh1231 <maxhumpherys@gmail.com>
2022-02-24 19:35:57 -0700

    email bug fix

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,7 +9,7 @@ const Contact = () => {
     const form = useRef();
 
     const sendEmail = (e) => {
-        e.preventDefault();
+        e.preventDefault()
         setView(true);
         emailjs.sendForm('service_zirnhu8', 'template_z5s2y9b', form.current, 'user_MiPMA7RruofT1yJEswkdw')
             .then((result) => {
@@ -29,28 +29,26 @@ const Contact = () => {
 
     return (
         <section className="flex flex-col justify-center items-center h-5/6 bg-[#e5e5e5]">
-            <h3 className="text-3xl text-[#4D0823]">Send me an email!</h3>
+            <h3 className="text-3xl text-[#4D0823]">Send me an Email!</h3>
             <div>
-                <form ref={form} onSubmit={handleSubmit(sendEmail)}>
+                <form ref={form} onSubmit={sendEmail}>
                     <div className="my-4 w-[400px] ">
-                        <input className="contact py-2 pl-2 w-full" type='text' placeholder='Name' name='from_name' {...register('name', { required: true })} />
-                        {errors.name && <span className="block bg-[#e5e5e5] text-[#4D0823]">This field is required.</span>}
+                        <input className="contact py-2 pl-2 w-full" type='text' placeholder='Your name' name='from_name'></input>
+                        {/* {errors.name && <span className="block bg-[#e5e5e5] text-[#4D0823]">This field is required.</span>} */}
 
 
                     </div>
                     <div className="my-4 w-[400px]">
-                        <input className="contact py-2 pl-2 w-full" type='text' placeholder='Email' name='from_email'
-                            {...register('email', { required: true, pattern: /\S+@\S+\.\S+/ })} />
-                        {errors.name && <span className="block bg-[#e5e5e5] text-[#4D0823]">Must be a valid email address.</span>}
+                        <input className="contact py-2 pl-2 w-full" type='text' placeholder='Your email' name='from_email'></input>
+                        {/* {errors.name && <span className="block bg-[#e5e5e5] text-[#4D0823]">Must be a valid email address.</span>} */}
                     </div>
                     <div className="my-4 w-[400px] h-[150px] shadow-lg">
-                        <textarea className="contact py-2 pl-2 w-full h-full" placeholder="Message" name='message'
-                            {...register('message', { required: true })} />
-                        {errors.message && <span className="block bg-[#e5e5e5] text-[#4D0823]">Please provide a message.</span>}
+                        <textarea className="contact py-2 pl-2 w-full h-full" placeholder="Message" name='message' ></textarea>
+                        {/* {errors.message && <span className="block bg-[#e5e5e5] text-[#4D0823]">Please provide a message.</span>} */}
 
                     </div>
                     <div className="my-4">
-                        <button className="mt-4 btn py-2 px-6 bg-[#FFF] ">Submit</button>
+                        <input type="submit" className="mt-4 btn py-2 px-6 bg-[#FFF]" value="Submit" />
                     </div>
                 </form>
             </div>
